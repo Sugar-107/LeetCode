00bf870ebd3cfad080a6e4d5e370020600ffbfc1
佛系少年要吃糖 <1074953453@qq.com>
2020-11-28 08:33:43 +0800

    2020年11月27日 19:09:09

--- day01/src/com/bilibili/Test01.java
+++ day01/src/com/bilibili/Test01.java
@@ -1,9 +1,32 @@
 package com.bilibili;
 
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Test01 {
 
 
     public static void main(String[] args) {
-        System.out.println(1 << 30);
+        MyRouse myRouse = new MyRouse();
+//        ExecutorService service = Executors.newCachedThreadPool();
+        ThreadPoolExecutor service = new ThreadPoolExecutor(5,10,2, TimeUnit.SECONDS, new SynchronousQueue<>(),new ThreadPoolExecutor.AbortPolicy());
+        // ... do something inside runnable task
+        for (int i = 0; i < 10; i++) {
+            service.execute(myRouse::showTicks);
+        }
+
+        service.shutdown();
+
     }
+
+
+}
+
+class MyRouse {
+    private AtomicInteger tickes = new AtomicInteger(30);
+    public void showTicks(){
+        System.out.println(Thread.currentThread().getName()+" "+tickes.decrementAndGet());
+        System.out.println(Thread.currentThread().getName()+" "+tickes.decrementAndGet());
+    }
+
 }
